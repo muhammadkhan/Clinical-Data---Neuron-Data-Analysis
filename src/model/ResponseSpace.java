7ce8aabd5ad6857ce64190608a6e6a10ac359c3c
Muhammad Khan <mhk98@cornell.edu>
2013-02-18 22:17:49 -0500

    i think i finished responsespace

--- src/model/ResponseSpace.java
+++ src/model/ResponseSpace.java
@@ -1,14 +1,19 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ResponseSpace{
 	
 	private ArrayList<double[]> c;
 	private double[][] distances;
+	private Map<double[], Integer> knnMap;
 	
 	public ResponseSpace(ArrayList<double[]> spiketrains){
 		c = spiketrains;
+		knnMap = new HashMap<double[], Integer>();
 		computeDistances();
 	}
 	
@@ -21,9 +26,28 @@ public class ResponseSpace{
 		}
 	}
 	
-	public void addTrain(double[] t){
+	public void testTrain(double[] t){
 		c.add(t);
 		computeDistances();
+		Double[] tt = new Double[t.length];
+		for(int i = 0; i < t.length; i++)
+			tt[i] = t[i];
+		int i = 0;
+		while(true){
+			Double[]  dd = new Double[distances[i].length];
+			for(int j = 0; j < distances[i].length; j++)
+				dd[j] = distances[i][j];
+			if(Arrays.deepEquals(tt, dd));
+				break;
+		}
+		//i will be set equal to t
+		int count = 0;
+		double threshold = .5;
+		for(int j = 0; j < c.size(); j++){
+			if(distances[i][j] <= threshold)
+				count++;
+		}
+		knnMap.put(t, count);
 	}
 }
 
